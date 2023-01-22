bb16ca6bfd37a5fa876915f66f8fae92cf98d006
loc-admin <you@example.com>
2023-01-22 14:34:05 +0300

    Sum

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,5 +1,5 @@
 ﻿int Num1 = new Random().Next(1, 10);// 1 2 3 4 ... 9
 Console.WriteLine(Num1);
-int Num2 = new Random().Next(1, 10);
+int Num2 = new Random().Next(1, 15);
 Console.WriteLine(Num2);
 Console.WriteLine(Num1 + Num2);
